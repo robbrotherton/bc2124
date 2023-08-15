32db791441765121b83cc4cc565dfacc11a389c1
robbrotherton <robert.brotherton@gmail.com>
2023-08-15 14:52:59 -0400

    updates bts ratings script

--- tipi_candidate_ratings.r
+++ tipi_candidate_ratings.r
@@ -5,7 +5,7 @@ data(timeseries_2016)
 
 df <- timeseries_2016 |>
   select(V162333:V162342, V162078, V162079) |>
-  filter(if_all(everything(), ~ . >= 0)) |>
+  filter(if_all(everything(), ~ . >= 0 & . <= 100)) |>
   mutate(extraversion_mean = (V162333 + (8 - V162338)) / 2,
          agreeableness_mean = ((8 - V162334) + V162339) / 2,
          conscientiousness_mean = (V162335 + (8 - V162340)) / 2,
@@ -13,6 +13,13 @@ df <- timeseries_2016 |>
          openness_mean = (V162337 + (8 - V162342)) / 2,) |>
   select(contains("mean"), V162078, V162079)
 
+df |>
+  ggplot(aes(x = V162078)) +
+  geom_histogram(binwidth = 1)
+
+df |>
+  ggplot(aes(x = V162079)) +
+  geom_histogram(binwidth = 1)
 
 cor(df)
 
